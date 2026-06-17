5617281d6139cc52285f0ca9ac8d73a9f3eb0cda
PhilippRitzberger <philipp@codat.at>
2026-06-17 16:44:04 +0200

    [BMP] base_search_mail_content: bump module to `19.*`

--- base_search_mail_content/__manifest__.py
+++ base_search_mail_content/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Base Search Mail Content",
-    "version": "17.0.1.1.0",
+    "version": "19.0.1.1.0",
     "author": "ForgeFlow, SerpentCS, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "category": "Social",
